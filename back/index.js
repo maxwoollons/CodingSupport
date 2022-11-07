fd89332a989371e422725c973314c64daa29edad
Max Woollons <maxwoollons@outlook.com>
2022-11-07 15:23:25 +1000

    init

---
+++ back/index.js
@@ -0,0 +1,15 @@
+//simple express app
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json('Hello World');
+})
+
+app.listen(5000, () => console.log('Server started on port 5000'));
+
+
+
+
+
---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-xl">Hello world</h1>
+  
+    </div>
+  )
+}
+
+export default App
